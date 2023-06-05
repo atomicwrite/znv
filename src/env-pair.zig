6271ff71339e2527773be7d8b5f67108f165999c
Ender <sterlingkooshesh@gmail.com>
2023-06-05 18:32:38 -0500

    Value test working
    key test now broken

--- src/env-pair.zig
+++ src/env-pair.zig
@@ -48,10 +48,15 @@ pub const EnvPair = struct {
     valueIndex: u8 = 0,
     interpolation: bool = false,
 
-    pub fn placeValueCharacter(self: *Self, value: u8) void {
+    fn placeValueCharacter(self: *Self, value: u8) void {
         // todo: check for overflow and resize array.
         self.value[self.valueIndex] = value;
         self.valueIndex = self.valueIndex + 1;
+    }
+    fn placeKeyCharacter(self: *Self, value: u8) void {
+        // todo: check for overflow and resize array.
+        self.key[self.valueIndex] = value;
+        self.keyIndex = self.keyIndex + 1;
     }
     pub fn previousIsSingleQuote(self: *Self) !bool {
         if (self.valueIndex == 0) {
@@ -65,20 +70,38 @@ pub const EnvPair = struct {
         }
         return self.value[self.valueIndex - 4] == '\n';
     }
-    pub fn previousDoubleQuoteCount(self: *Self) !u8 {
+    pub fn previousSingleQuoteCount(self: *Self) u8 {
         var tmp = self.valueIndex;
-        var count = 0;
+        var count: u8 = 0;
         while (tmp > 0) {
-            if (self.value[tmp - 1] == '"') {
+            if (self.value[tmp - 1] == '\'') {
                 count = count + 1;
+            }
+            if (count >= 3) {
+                //avoid """"""""""""""""""""""""
+                break;
             }
         }
         return count;
     }
-    pub fn isAtStart(self: *Self) u8 {
+    pub fn previousDoubleQuoteCount(self: *Self) u8 {
+        var tmp = self.valueIndex;
+        var count: u8 = 0;
+        while (tmp > 0) {
+            if (self.value[tmp - 1] == '"') {
+                count = count + 1;
+            }
+            if (count >= 3) {
+                //avoid """"""""""""""""""""""""
+                break;
+            }
+        }
+        return count;
+    }
+    pub fn isAtStart(self: *Self) bool {
         return self.valueIndex == 0;
     }
-    pub fn fourCharactersAgoStart(self: *Self) u8 {
+    pub fn fourCharactersAgoStart(self: *Self) bool {
         return self.valueIndex == 3;
     }
     pub fn previousIsDollarSign(self: *Self) bool {
@@ -91,7 +114,12 @@ pub const EnvPair = struct {
         if (self.quoted) { // process value if we have detected this is a single quoted value
             return self.processValueInsideQuoted(value);
         }
-
+        if (value == '\'') { //single quote
+            if (self.isAtStart()) { //at the start
+                self.quoted = true;
+                return false;
+            }
+        }
         if (value == '{') { //ok we have either no double quote or are double quoted, process if we find ${}
             if (self.previousIsDollarSign()) {
                 self.envValueCounter.incrementInterpolDepth();
@@ -104,10 +132,7 @@ pub const EnvPair = struct {
                 return false;
             }
         }
-        if(value == '\''){
-            //todo: wrote a processSingleQuote similar to processDoubleQuote to handle end/start
-            return false;
-        }
+
         // ok so it's not a single quoted string and not a quote
         if (value != '"') {
             //todo: check for escape (prob in placeValueCharacter)
@@ -115,9 +140,9 @@ pub const EnvPair = struct {
             return false;
         }
         // ok it's a double quote
-        return self.processDoubleQuote(value);
+        return self.processDoubleQuote();
     }
-    fn processDoubleQuote(self: *Self, value: u8) !bool {
+    fn processDoubleQuote(self: *Self) bool {
         const streak = self.previousDoubleQuoteCount();
 
         if (streak == 0) {
@@ -140,11 +165,21 @@ pub const EnvPair = struct {
         }
         if (self.fourCharactersAgoStart()) {
             self.tripleDoubleQuoted = true;
-            return false;
         }
+        return false;
     }
     pub fn processValueInsideQuoted(self: *Self, value: u8) !bool {
         if (value == '\'') {
+            const streak = self.previousSingleQuoteCount();
+            if (streak == 3) {
+                if (self.fourCharactersAgoStart()) {
+                    self.tripleQuoted = true;
+                    return false;
+                }
+                if (self.fourCharactersAgoNewline()) {
+                    return true;
+                }
+            }
             return true;
         }
         if (value == '\r') {
--- src/env-reader.zig
+++ src/env-reader.zig
@@ -3,50 +3,45 @@ const std = @import("std");
 const Reader = std.fs.File.Reader;
 const EnvPair = @import("env-pair.zig").EnvPair;
 
-const EnvReaderError = error {
+const EnvReaderError = error{
     KeyStartedWithNumber,
     InvalidKeyCharacter,
 };
-  pub fn nextValue(  reader: Reader, pair: *EnvPair, ) !void {
+pub fn nextValue(
+    reader: Reader,
+    pair: *EnvPair,
+) !void {
+    var end = false;
 
+    while (!end) {
+        const byte = reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => |e| return e,
+        };
 
-      while (true) {
-         const byte = reader.readByte() catch |err| switch (err) {
-             error.EndOfStream => break,
-             else => |e| return e,
-         };
-
-          pair.processValueNextValue(byte) catch |err| switch (err) {
-           error.InvalidKeyCharacter => break,
-           error.KeyStartedWithNumber => break,
-         };
-
-
-      }
-
-
-
+        end = pair.processValueNextValue(byte) catch |err| switch (err) {
+            error.InvalidKeyCharacter => break,
+            error.KeyStartedWithNumber => break,
+        };
     }
+}
 
-      pub fn nextKey(  reader: Reader, pair: *EnvPair, ) !void {
-
-
-      while (true) {
-         const byte = reader.readByte() catch |err| switch (err) {
-             error.EndOfStream => break,
-             else => |e| return e,
-         };
+pub fn nextKey(
+    reader: Reader,
+    pair: *EnvPair,
+) !void {
+    while (true) {
+        const byte = reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => |e| return e,
+        };
         const isLastKeyValue = pair.processKeyNextValue(byte) catch |err| switch (err) {
-           error.InvalidKeyCharacter => break,
-           error.KeyStartedWithNumber => break,
-         };
-               std.debug.print("Is Last? :  {any}   \n", .{isLastKeyValue});
-               if(isLastKeyValue){
-                break;
-               }
-      }
-
-
+            error.InvalidKeyCharacter => break,
+            error.KeyStartedWithNumber => break,
+        };
+        std.debug.print("Is Last? :  {any}   \n", .{isLastKeyValue});
+        if (isLastKeyValue) {
+            break;
+        }
     }
-
-
+}
